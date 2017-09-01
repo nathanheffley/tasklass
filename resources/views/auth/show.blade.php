bdd811d90f1fcac84f14c4bbc4a7edb4a750e9f6
Nathan Heffley <nathanheffley@gmail.com>
2017-08-31 21:06:27 -0400

    Show errors and style login page

--- resources/views/auth/show.blade.php
+++ resources/views/auth/show.blade.php
@@ -1,10 +1,40 @@
 @extends('layouts.master')
 
 @section('body')
-    <form action="/login" method="POST">
-        {{ csrf_field() }}
-        <input type="email" name="email" placeholder="you@example.com">
-        <input type="password" name="password" placeholder="password">
-        <button type="submit">Log In</button>
-    </form>
+    <section class="section">
+        <div class="container">
+            <form action="/login" method="POST">
+                {{ csrf_field() }}
+
+                <div class="field">
+                    <label class="label">Email</label>
+                    <div class="control has-icons-left{{ $errors->has('email') ? ' has-icons-right' : '' }}">
+                        <input class="input{{ $errors->has('email') ? ' is-danger' : '' }}"
+                            type="email" name="email" placeholder="you@example.com" value="{{ old('email') }}" required>
+                        <span class="icon is-small is-left"><i class="fa fa-envelope"></i></span>
+                        @if ($errors->has('email'))
+                            <span class="icon is-small is-right"><i class="fa fa-warning"></i></span>
+                            <p class="help is-danger">{{ $errors->first('email') }}</p>
+                        @endif
+                    </div>
+                </div>
+
+                <div class="field">
+                    <label class="label">Password</label>
+                    <div class="control">
+                        <input class="input" type="password" name="password" placeholder="password" required>
+                    </div>
+                </div>
+
+                <div class="field is-grouped">
+                    <div class="control">
+                        <button class="button is-primary" type="submit">Log in</button>
+                    </div>
+                    <div class="control">
+                        <a class="button is-link" href="/register">Need an Account?</a>
+                    </div>
+                </div>
+            </form>
+        </container>
+    </section>
 @endsection
