f41e7c160f114666e757271aca92731673665b5e
Nathan Heffley <nathanheffley@gmail.com>
2017-09-06 14:53:45 +0000

    Add manifest to start PWA

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,6 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="theme-color" content="#00d1b2">
         <title>@yield('title', 'Todo')</title>
 
         <script>
@@ -13,6 +14,7 @@
             };
         </script>
         <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
+        <link rel="manifest" href="/manifest.json">
     </head>
     <body>
         @include('layouts.navbar')
